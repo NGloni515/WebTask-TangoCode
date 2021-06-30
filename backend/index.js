b14bb5d871e9c878d232b6986bfe423f32911839
NGloni515 <angelonischolar@gmail.com>
2021-06-30 15:35:00 -0500

    cleaning backend code

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,6 @@ app.use(express.json())
 
 app.use(carRouter)
 
-app.use(express.static(path.join(__dirname, '../web-task/build')));
-
-app.use('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/build', 'index.html')));
-
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
